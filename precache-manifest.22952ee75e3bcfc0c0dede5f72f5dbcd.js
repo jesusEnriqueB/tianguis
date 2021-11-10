93d864bf3f5440363a6a44d2d122da6e0705997e
jesus0696 <jesus.balam@hiumanlab.com>
2021-11-10 05:42:17 -0600

    tianguis

---
+++ precache-manifest.22952ee75e3bcfc0c0dede5f72f5dbcd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6049f91222140e71ee4e967f8cac013c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e94be3e64e0bec3f3e4",
+    "url": "/static/css/2.40572fe1.chunk.css"
+  },
+  {
+    "revision": "4562437cb09f2841b6d6",
+    "url": "/static/css/main.c31dd5b8.chunk.css"
+  },
+  {
+    "revision": "3e94be3e64e0bec3f3e4",
+    "url": "/static/js/2.c940d5f3.chunk.js"
+  },
+  {
+    "revision": "e618d9073ae56f2ec347",
+    "url": "/static/js/3.6b25de6b.chunk.js"
+  },
+  {
+    "revision": "b7510c0a4bc63ea7052d",
+    "url": "/static/js/4.04e5214a.chunk.js"
+  },
+  {
+    "revision": "8a2028831801ecb448ad",
+    "url": "/static/js/5.3f91b144.chunk.js"
+  },
+  {
+    "revision": "4562437cb09f2841b6d6",
+    "url": "/static/js/main.e12ee417.chunk.js"
+  },
+  {
+    "revision": "3e9d4eed898ee9208bf7",
+    "url": "/static/js/runtime~main.0ae4aaa8.js"
+  },
+  {
+    "revision": "e2ce384d6be29119a4bb477973d1a1ac",
+    "url": "/static/media/TURISMO_horizontal.e2ce384d.png"
+  },
+  {
+    "revision": "708e4cbfa214afcfbfa0f85f0d755e35",
+    "url": "/static/media/footer.708e4cbf.png"
+  },
+  {
+    "revision": "d66c21aa8147384993ce273fc0e353f8",
+    "url": "/static/media/header.d66c21aa.png"
+  }
+]);
